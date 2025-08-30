922f618bf50b09b4ea013aaac592b4e2479c512f
rohitsh7705-jpg <rohit.sh7705@gmail.com>
2025-08-30 06:51:49 +0000

    done

---
+++ 02array.js
@@ -0,0 +1,19 @@
+const marvelheroes = ["thor","ironman","spiderman"]
+const dc= ["superman", "flash", "spiderman", "batman"]
+//marvelheroes.push(dc)
+// console.log(marvelheroes)
+// console.log(marvelheroes[3][1])
+// const allheroes =marvelheroes.concat(dc)
+// console.log(allheroes)
+//const allnewheros ={...marvelheroes,...dc}
+//console.log(allnewheros)
+// const anotherarr =[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const realarr =anotherarr.flat(Infinity)
+// console.log(realarr)
+console.log(Array.isArray("DIvya"))
+console.log(Array.from("DIvya"))
+console.log(Array.from({name :"DIvya"}))
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3))
