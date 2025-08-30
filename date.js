5566f121e8e043a7bb67ccb489537f2c873d8269
rohitsh7705-jpg <rohit.sh7705@gmail.com>
2025-08-30 06:31:25 +0000

    done

---
+++ date.js
@@ -0,0 +1,17 @@
+//Dates
+let mydate = new Date()
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+
+console.log(typeof mydate)
+//let mycreatedate = new  Date(2023,0,23)
+//let mycreatedate =new Date(2023,0,23,5,45)
+let mycreatedate=new Date("01-14-2025")
+
+//console.log(mycreatedate.toLocaleString())
+let mytimestamp=  Date.now()
+// console.log(mytimestamp)
+// console.log(mycreatedate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
